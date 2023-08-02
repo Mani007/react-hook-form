c85c839ccc3a5702e61d3f80a2a33ad312295081
Mohnish <mohnish.a007@gmail.com>
2023-08-02 01:43:49 +0100

    Social Login without react form

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Basic_login from "./components/Basic_login";
+import Social_login from "./components/Social_login";
 
 export default function App() {
  
 
   return (
    <>
-   <Basic_login/>
+   {/* <Basic_login/> */}
+   <Social_login/>
    </>
   );
 }
